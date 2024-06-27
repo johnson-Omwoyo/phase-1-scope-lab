276ce4170b4930aae02e824a0e848b5309c52b91
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-06-27 14:28:02 +0300

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "Khaled"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+     bestCustomer  = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "trying to change const value"
+}
